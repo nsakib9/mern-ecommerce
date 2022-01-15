0a245b9210b7a815564a8763934aa75b0d1d7266
nsakib9 <sakibkhan207@gmail.com>
2022-01-15 13:46:03 +0600

    Copy data from git to data/product.js

---
+++ backend/data/products.js
@@ -0,0 +1,101 @@
+const ProductData = [
+    {
+      name: 'Bambi Print Mini Backpack 1',
+      image: '/images/product1.jpg',
+      price: 150,
+      rating: 4,
+      numReviews: 4,
+      countInStock: 5,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 2',
+      image: '/images/product2.jpg',
+      price: 150,
+      rating: 3.5,
+      numReviews: 4,
+      countInStock: 2,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 3',
+      image: '/images/product3.jpg',
+      price: 150,
+      rating: 5,
+      numReviews: 4,
+      countInStock: 8,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 4',
+      image: '/images/product4.jpg',
+      price: 150,
+      rating: 2.5,
+      numReviews: 4,
+      countInStock: 0,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 5',
+      image: '/images/product5.jpg',
+      price: 150,
+      rating: 4,
+      numReviews: 4,
+      countInStock: 2,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 6',
+      image: '/images/product6.jpg',
+      price: 150,
+      rating: 5,
+      numReviews: 4,
+      countInStock: 0,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 7',
+      image: '/images/product7.jpg',
+      price: 150,
+      rating: 1.5,
+      numReviews: 4,
+      countInStock: 10,
+      brand: 'apple',
+      category: 'electronics',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+    {
+      name: 'Bambi Print Mini Backpack 8',
+      image: '/images/product8.jpg',
+      price: 150,
+      rating: 3,
+      numReviews: 4,
+      brand: 'apple',
+      category: 'electronics',
+      countInStock: 5,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cupiditate totam? Cum, accusamus placeat dicta reiciendis nobis libero fugiat quam.',
+    },
+  ]
+  
+  export default ProductData
+  

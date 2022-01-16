71181430c24c788e373b3c4e3c17fc3010d6f275
nsakib9 <sakibkhan207@gmail.com>
2022-01-16 08:06:07 +0600

    Create productRoutes functions

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// GET all products from db
+// GET /api/products
+// Public Access
+router.get('/', (req, res)=> {
+
+})
+
+// GET one products from db
+// GET /api/products/:id
+// Public Access
+router.get('/:id', (req, res)=> {
+
+})
+
+module.exports = router;

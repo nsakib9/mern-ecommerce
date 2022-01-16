fbc77f08d9975482fbbf1c826828064dcc26124e
nsakib9 <sakibkhan207@gmail.com>
2022-01-17 00:15:06 +0600

    Import files, Fix Routes and check by run

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from  'react-router-dom';
+import {BrowserRouter as Router, Routes, Switch, Route} from  'react-router-dom';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
@@ -10,11 +15,11 @@ function App() {
      {/* SideDrawar */}
      {/* Backdrop */}
      <main>
-       <Switch>
-         <Route exact path="/" component={HomeScreen} />
-         <Route exact path="/product/:id" component={ProductScreen} />
-         <Route exact path="/cart" component={CartScreen} />
-       </Switch>
+       <Routes>
+         <Route exact path="/" element={<HomeScreen />} />
+         <Route exact path="/product/:id" element={<ProductScreen/>} />
+         <Route exact path="/cart" element={<CartScreen/>} />
+       </Routes>
      </main>
     </div>
     </Router>

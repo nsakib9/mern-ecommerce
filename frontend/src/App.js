b23dab1ea3e6f281e9251db20031192037d3c9e1
nsakib9 <sakibkhan207@gmail.com>
2022-01-16 23:27:25 +0600

    Create new Route and warp with Router and Switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from  'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
      {/* Navbar */}
      {/* SideDrawar */}
      {/* Backdrop */}
-     {/* HomeScreen */}
-     {/* ProductScreen */}
-     {/* CartScreen */}
+     <main>
+       <Switch>
+         <Route exact path="/" component={HomeScreen} />
+         <Route exact path="/product/:id" component={ProductScreen} />
+         <Route exact path="/cart" component={CartScreen} />
+       </Switch>
+     </main>
     </div>
+    </Router>
   );
 }
 

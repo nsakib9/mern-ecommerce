6d4293cc13075b51567f839aedbaaba408bd9fd8
nsakib9 <sakibkhan207@gmail.com>
2022-01-19 15:37:21 +0600

    BS import, Replace Navbar with Bootstrap structure

--- Readme.md
+++ Readme.md
@@ -9,3 +9,7 @@ create-react-app .
 
 for frontend nav and route:
 npm i axios react-router-dom redux react-redux redux-thunk redux-devtools-extension
+
+
+After this commit: Navbar text add, import to App, font awesome add
+Add bootstrap
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,35 +1,41 @@
-import './Navbar.css'
-import {Link} from 'react-router-dom'
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className ="navbar" >
-            {/* logo */}
-            <div className="navbar__logo">
-                <h2>Shopping Cart MERN</h2>
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <div className="container-fluid">
+          <h2 className="text-white">MERN Ecommerce</h2>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <li className="nav-item">
+            <Link className="nav-link" to="/shop">Shop</Link>
+            </li>
+          </ul>
+
+            <div className="w-100 d-flex justify-content-end">
+              <Link to="/cart">
+                <i className="fas fa-shopping-cart"></i>
+                <button type="button" class="btn btn-primary">
+                  Cart <span class="badge bg-secondary">0</span>
+                </button>
+              </Link>
             </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-            {/* links */}
-            <ul className="navbar__links">
-                <li>
-                    <Link to="/cart">
-                        <i className='fas fa-shopping-cart'></i>
-                        Cart
-                        <span className='cartlogo__badge'>0</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/shop">Shop</Link>
-                </li>
-            </ul>
-
-            <div className='hanburger__menu'>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
